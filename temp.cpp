b8ee792d05101e9885d5f6a91c8f76e823ace810
shimohara-svg <61421749+shimohara-svg@users.noreply.github.com>
2021-12-05 20:51:24 +0900

    Create temp.cpp

---
+++ temp.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <queue>
+#include <map>
+#include <cmath>
+#include <iomanip>
+#include <set>
+#include <cstdio>
+#include <string>
+
+using namespace std;
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  //cout << fixed << setprecision(15);
+  
+
+  return 0;
+}
